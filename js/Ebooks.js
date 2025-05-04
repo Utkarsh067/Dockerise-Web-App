20550d61b25d00135648869d6901bce816cf953a
Utkarsh Jain <161854515+Utkarsh067@users.noreply.github.com>
2025-05-05 00:16:25 +0530

    Update Ebooks.js

--- js/Ebooks.js
+++ js/Ebooks.js
@@ -3,77 +3,75 @@ document.addEventListener('DOMContentLoaded', () => {
         {
             title: 'The Kite Runner',
             description: 'A coming-of-age story set in Afghanistan, exploring themes of friendship, betrayal, guilt, and redemption.',
-            image: 'image/the-kite-runner.jpg', // Add path to the image
-            fileUrl: 'https://virtualebooks.s3.ap-south-1.amazonaws.com/the-kite-runner-pdf-by-indianpdf.com_.pdf' // Add S3 URL
+            image: 'image/the-kite-runner.jpg', 
+            fileUrl: 'https://virtualebooks.s3.ap-south-1.amazonaws.com/the-kite-runner-pdf-by-indianpdf.com_.pdf' 
         },
         {
             title: 'Atomic Habits',
             description: '"Atomic Habits" offers a practical guide to building good habits and breaking bad ones through small, incremental changes.',
-            image: 'image/atomic-habits.jpeg', // Add path to the image
-            fileUrl: 'https://virtualebooks.s3.ap-south-1.amazonaws.com/Atomic+Habits.pdf' // Add S3 URL
+            image: 'image/atomic-habits.jpeg', 
+            fileUrl: 'https://virtualebooks.s3.ap-south-1.amazonaws.com/Atomic+Habits.pdf'
         },
         {
             title: 'Eat that Frog',
             description: '"Eat That Frog" emphasizes tackling your most challenging task first for a productive and fulfilling day.',
-            image: 'image/eat-that-frog.jpeg', // Add path to the image
-            fileUrl: 'https://virtualebooks.s3.ap-south-1.amazonaws.com/Eat-that-frog-by-brian-tracy-pdf-book.pdf' // Add S3 URL
+            image: 'image/eat-that-frog.jpeg', 
+            fileUrl: 'https://virtualebooks.s3.ap-south-1.amazonaws.com/Eat-that-frog-by-brian-tracy-pdf-book.pdf' 
         },
         {
             title: 'How to win Friends and Influence people',
-            description: '"Dale Carnegie,s" classic offers advice on building positive relationships and persuasion through empathy and effective communication.',
-            image: 'image/how to win.jpeg', // Add path to the image
-            fileUrl: 'https://virtualebooks.s3.ap-south-1.amazonaws.com/how-to-win-friends-and-influence-people.pdf' // Add S3 URL
+            description: '"Dale Carnegie`s" classic offers advice on building positive relationships and persuasion through empathy and effective communication.',
+            image: 'image/how to win.jpeg', 
+            fileUrl: 'https://virtualebooks.s3.ap-south-1.amazonaws.com/how-to-win-friends-and-influence-people.pdf' 
         },
         {
             title: 'Ikigai',
-            description: '"Ikigai" by Hector Garcia and Francesc Miralles delves into the Japanese concept of finding one,s life purpose, offering insights into longevity, happiness, and the pursuit of meaningful existence through practical guidance and philosophical reflections.',
-            image: 'image/ikigai.jpeg', // Add path to the image
-            fileUrl: 'https://virtualebooks.s3.ap-south-1.amazonaws.com/Ikigai.pdf' // Add S3 URL
+            description: '"Ikigai" by Hector Garcia and Francesc Miralles delves into the Japanese concept of finding one`s life purpose, offering insights into longevity, happiness, and the pursuit of meaningful existence through practical guidance and philosophical reflections.',
+            image: 'image/ikigai.jpeg', 
+            fileUrl: 'https://virtualebooks.s3.ap-south-1.amazonaws.com/Ikigai.pdf' 
         },
         {
-            title: 'Man,s Search For a Meaning',
+            title: 'Man`s Search For a Meaning',
             description: 'Viktor Frankl explores finding purpose and meaning in life, even amidst suffering, based on his experiences in Nazi concentration camps.',
-            image: 'image/man search.jpeg', // Add path to the image
-            fileUrl: 'https://virtualebooks.s3.ap-south-1.amazonaws.com/Mans+Search+for+Meaning+-+Viktor+E.+Frankl+%5BWorldfreebooks.com%5D.pdf' // Add S3 URL
+            image: 'image/man search.jpeg', 
+            fileUrl: 'https://virtualebooks.s3.ap-south-1.amazonaws.com/Mans+Search+for+Meaning+-+Viktor+E.+Frankl+%5BWorldfreebooks.com%5D.pdf'
         },
         {
             title: 'Power of your Subconscious mind',
-            description: 'Joseph Murphy,s book suggests harnessing the subconscious mind,s power for positive change and achieving your goals.',
-            image: 'image/power of subconscious mind.jpeg', // Add path to the image
-            fileUrl: 'https://virtualebooks.s3.ap-south-1.amazonaws.com/power-subconscious-mind.pdf' // Add S3 URL
+            description: 'Joseph Murphy`s book suggests harnessing the subconscious mind`s power for positive change and achieving your goals.',
+            image: 'image/power of subconscious mind.jpeg',
+            fileUrl: 'https://virtualebooks.s3.ap-south-1.amazonaws.com/power-subconscious-mind.pdf'
         },
         {
             title: 'Rich dad Poor dad',
             description: '"Rich Dad Poor Dad" contrasts two financial mindsets, advocating financial literacy and building wealth through assets and investing.',
-            image: 'image/rich-dad-poor-dad.jpeg', // Add path to the image
-            fileUrl: 'https://virtualebooks.s3.ap-south-1.amazonaws.com/Rich+Dad+Poor+Dad.pdf' // Add S3 URL
+            image: 'image/rich-dad-poor-dad.jpeg', 
+            fileUrl: 'https://virtualebooks.s3.ap-south-1.amazonaws.com/Rich+Dad+Poor+Dad.pdf'
         },
         {
             title: 'Sapiens - a brief history of mankind',
-            description: 'Yuval Noah Harari explores humanity,s rise from insignificant animal to dominant species through cooperation and unique cognitive abilities.',
-            image: 'image/sapiens.jpeg', // Add path to the image
-            fileUrl: 'https://virtualebooks.s3.ap-south-1.amazonaws.com/Sapiens-A-Brief-History-of-Humankind.pdf' // Add S3 URL
+            description: 'Yuval Noah Harari explores humanity`s rise from insignificant animal to dominant species through cooperation and unique cognitive abilities.',
+            image: 'image/sapiens.jpeg', 
+            fileUrl: 'https://virtualebooks.s3.ap-south-1.amazonaws.com/Sapiens-A-Brief-History-of-Humankind.pdf'
         },
         {
             title: 'The subtle art of not giving a Fu*k',
-            description: 'Mark Manson,s book challenges traditional self-help by arguing that focusing on what truly matters allows for happiness and personal growth.',
-            image: 'image/subtle art.jpeg', // Add path to the image
-            fileUrl: 'https://virtualebooks.s3.ap-south-1.amazonaws.com/the+subtle+art+of+not+giving+a+fuck.pdf' // Add S3 URL
+            description: 'Mark Manson`s book challenges traditional self-help by arguing that focusing on what truly matters allows for happiness and personal growth.',
+            image: 'image/subtle art.jpeg', 
+            fileUrl: 'https://virtualebooks.s3.ap-south-1.amazonaws.com/the+subtle+art+of+not+giving+a+fuck.pdf' 
         },
         {
             title: 'The 48 Laws of Power',
             description: '"The 48 Laws of Power" by Robert Greene offers ruthless tactics for navigating social hierarchies and gaining influence.',
-            image: 'image/Power 48 laws.png', // Add path to the image
-            fileUrl: 'https://virtualebooks.s3.ap-south-1.amazonaws.com/The-48-Laws-of-Power-Robert-Greene.pdf' // Add S3 URL
+            image: 'image/Power 48 laws.png', 
+            fileUrl: 'https://virtualebooks.s3.ap-south-1.amazonaws.com/The-48-Laws-of-Power-Robert-Greene.pdf' 
         },
         {
             title: 'Think and Grow rich',
-            description: 'Napoleon Hill,s "Think and Grow Rich" emphasizes the power of desire, belief, and persistence in achieving financial success.',
-            image: 'image/think and grow rich.jpeg', // Add path to the image
-            fileUrl: 'https://virtualebooks.s3.ap-south-1.amazonaws.com/Think-And-Grow-Rich_2011-06.pdf' // Add S3 URL
+            description: 'Napoleon Hill`s "Think and Grow Rich" emphasizes the power of desire, belief, and persistence in achieving financial success.',
+            image: 'image/think and grow rich.jpeg', 
+            fileUrl: 'https://virtualebooks.s3.ap-south-1.amazonaws.com/Think-And-Grow-Rich_2011-06.pdf' 
         },
-
-        // Add more books here
     ];
 
     const bookList = document.getElementById('book-list');
@@ -112,7 +110,7 @@ document.addEventListener('DOMContentLoaded', () => {
         on: {
             reachEnd: function () {
                 currentPage++;
-                loadBooks(currentPage); // Load more books on reach end
+                loadBooks(currentPage); 
             }
         }
     });
